f436bbe5edf6982d943ebdb21be6c470d8f36cd2
IsaacRenderos2109 <122133180+IsaacRenderos2109@users.noreply.github.com>
2024-10-15 10:28:34 -0600

    11

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function toggleMenu() {
         menu.style.display = "flex";
     }
 }
+
+/*Carrusel*/
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll(".carousel-images img");
+    const totalImages = images.length;
+    let currentIndex = 0;
+
+    function showNextImage() {
+        currentIndex = (currentIndex + 1) % totalImages;
+        document.querySelector(".carousel-images").style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    // Cambia de imagen cada 5 segundos
+    setInterval(showNextImage, 5000);
+});
